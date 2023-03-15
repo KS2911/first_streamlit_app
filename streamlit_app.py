efa853efd08b10029b0cc5cbc90ac5236ceeca7e
KS2911 <119046296+KS2911@users.noreply.github.com>
2023-03-15 10:49:40 +0530

    Update streamlit_app.py
    Added breakfast menu text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents new healty dinner')
+streamlit.headers('Breakfast Menu')
+streamlit.text('Parantha')
+streamlit.text('chole puri')
+streamlit.text('Dosa')
