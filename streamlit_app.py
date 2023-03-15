8a08f4f67cbc3a969be1e8adb212a1c21960d231
KS2911 <119046296+KS2911@users.noreply.github.com>
2023-03-15 10:50:23 +0530

    Update streamlit_app.py
    fixed header typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents new healty dinner')
-streamlit.headers('Breakfast Menu')
+streamlit.header('Breakfast Menu')
 streamlit.text('Parantha')
 streamlit.text('chole puri')
 streamlit.text('Dosa')
